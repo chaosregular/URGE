94b6dc32d0d0b5bac098b8e1fb90c155f4ec152e
chaosregular <chaosregular@gmail.com>
2025-09-22 17:56:23 +0200

    Create beings_physics.c
    Seek from Seek_chaosregular_x_2025_09_22_13_31_30

---
+++ experiments/beings/beings_physics.c
@@ -0,0 +1,4 @@
+void being_update_internal_particles(Being* being);
+void being_compute_center_of_mass(Being* being);
+double being_get_property(Being* being, PropertyAxis axis);
+void being_adapt_to_environment(Being* being, WorldCell** world);
