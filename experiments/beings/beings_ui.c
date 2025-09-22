667aac696aed1f64bb9277ebbd3cba1793b87bec
chaosregular <chaosregular@gmail.com>
2025-09-22 17:57:45 +0200

    Create beings_ui.c
    Seek from Seek_chaosregular_x_2025_09_22_13_31_30

---
+++ experiments/beings/beings_ui.c
@@ -0,0 +1,3 @@
+void ui_draw_world(SDL_Renderer* renderer, WorldCell** world, int w, int h);
+void ui_draw_beings(SDL_Renderer* renderer, Being* beings, int count);
+void ui_draw_property_space(SDL_Renderer* renderer, Being* being, int x, int y);
